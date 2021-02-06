d8b18b837e9d86e088ac684bdd0f295a8e0425fc
Chitransh Jain <cjsmcs2810@gmail.com>
2021-02-06 23:05:55 +0530

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,27 +5,27 @@ import "../styles/Header.css"
 
 function Header() {
 
-  const [navbar, setNavbar] = useState("navbar");
+  const [navbar, setNavbar] = useState(false);
 
   function changeBackground() {
     if(window.scrollY>=90) {
-      setNavbar("navbar active");
+      setNavbar(true);
     } else {
-      setNavbar("navbar");
+      setNavbar(false);
     }
   }
 
   window.addEventListener("scroll", changeBackground);
 
-    return <div><Navbar fixed="top" className="navbar" collapseOnSelect expand="lg">
-    <Navbar.Brand className="navbar-brand" href="#home">Havrock Solutions</Navbar.Brand>
+    return <div><Navbar fixed="top" className={navbar? "navbar active" : "navbar"} collapseOnSelect expand="lg">
+    <Navbar.Brand className={navbar? "navbar-brands active" : "navbar-brands"} href="#home">Havrock Solutions</Navbar.Brand>
     <Navbar.Toggle className="nav-toggle" aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
       <div className="nav-items"><Nav className="mr-auto">
-        <Nav.Link className="nav-link" href="#home">Home</Nav.Link>
-        <Nav.Link href="#about">About Us</Nav.Link>
-        <Nav.Link href="#what">What We Do?</Nav.Link>
-        <Nav.Link href="#contact">Contact Us</Nav.Link>
+        <Nav.Link className={navbar? "nav-link active" : "nav-link"} href="#home">Home</Nav.Link>
+        <Nav.Link className={navbar? "nav-link active" : "nav-link"} href="#about">About Us</Nav.Link>
+        <Nav.Link className={navbar? "nav-link active" : "nav-link"} href="#what">What We Do?</Nav.Link>
+        <Nav.Link className={navbar? "nav-link active" : "nav-link"} href="#contact">Contact Us</Nav.Link>
       </Nav></div>
     </Navbar.Collapse>
   </Navbar>
